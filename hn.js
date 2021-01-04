27300cc8126fb79016d898a8b2f84a31652a0ae1
Paul Kohler <pkohler48@gmail.com>
2021-01-04 17:44:35 -0500

    Changed scale of the pdf
    scale set ti 0.5

--- hn.js
+++ hn.js
@@ -4,7 +4,7 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto('https://news.ycombinator.com', {waitUntil: 'networkidle2'});
-  await page.pdf({path: 'hn.pdf', format: 'A4'});
+  await page.pdf({path: 'hn.pdf', format: 'A4', scale: 0.5});
 
   await browser.close();
 })();
